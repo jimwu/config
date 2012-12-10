bbec0471587b4479a6012cd34014c44e54ead4da
Jim Wu <jimwu@kkbox.com>
2012-12-10 17:54:18 +0800

    ln 不要強制執行

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 base_dir=`dirname $0`
 umask 022
 
-ln -sf ${base_dir}/.bashrc ~/
-ln -sf ${base_dir}/.profile ~/
-ln -sf ${base_dir}/.screenrc ~/
+ln -s ${base_dir}/.bashrc ~/
+ln -s ${base_dir}/.profile ~/
+ln -s ${base_dir}/.screenrc ~/
